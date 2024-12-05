0a051407033b00baf1a69afed59e0b4233d04d3a
jbodosa <jessica.bodosa@gmail.com>
2024-12-05 14:14:46 -0500

    organizing add ions

---
+++ src/yank_template/README.md
@@ -0,0 +1,13 @@
+## Steps to generate the alchem setup
+
+<ol>
+  <li>Generate the moleucle/system with counter-ions using packmol > add_ions.sh</li>
+  <li>Generate the crd file for this ion box</li>
+  <li>Use the crd file to generate the water and vacuum boxes using CHARMM</li>
+
+```
+$PACKMOL <ion_box.inp >ion_box.out
+
+$CHARMM <ion_wat.inp >ion_wat.out
+$CHARMM <ion_vac.inp >ion_vac.out
+```
---
+++ src/yank_template/add_ions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+#
+
+## Is the molecule/system neutral ?
+# If not then add ions
+# Else exit with simple no
+## Run packmol script to add ions
+#First check the charge of molecule/system
+# Run in subprocess $PACKMOL <ion_box.inp >ion_box.out
+
+
+```
+    Check the syscharge file in the structre dir. If neutral molecule then don't add charge else check if positive
+        or negative and add correct counter ion (CLA/POT) or ask for counter-ion name.
+```
+
+# Imports
+import os
+import subprocess
+
+input_file = "../meso/syscharge.dat"
+
+class System:
+    def __init__(
+        self,
+        syscharge=0, # Assume neutral
+    )
+
+    def get_charge()
