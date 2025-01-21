f6ff3e5742ddf28493bef41850404bfaaf8df900
jbodosa <jessica.bodosa@gmail.com>
2025-01-21 15:53:20 -0500

    setup yank glp1-be

---
+++ beryparam/test_glp1_be/mk_waterbox.py
@@ -0,0 +1,146 @@
+import numpy as np
+from openmm.app import *
+from openmm import *
+from openmm.unit import *
+import parmed as pmd
+from openmm.app import Element
+
+# Read in the pdb
+pdb_input = "sys_vacuum.pdb"
+# Read in the psf
+psf_input = "sys_vacuum.psf"
+
+# Read in the convreted charmm forcefield
+forcefield = ForceField("charmm36.xml")
+
+# Load the pdb and psf files
+pdb = PDBFile(pdb_input)
+psf = CharmmPsfFile(psf_input)
+psf.setBox(3* nanometers, 3* nanometers, 3* nanometers, 90.0, 90.0, 90.0 )
+
+# Create the model with correct resnames from psf
+modeller = Modeller(psf.topology, pdb.positions)
+
+# Create OpenMM system
+system = forcefield.createSystem(
+    topology= modeller.topology,
+    nonbondedMethod=PME,
+    nonbondedCutoff=1.2 * nanometers,
+    switchDistance=1.0 * nanometers,
+    constraints=HBonds
+)
+
+nonbonded = [f for f in system.getForces() if isinstance(f, NonbondedForce)][0]
+
+charges = []
+for i in range(system.getNumParticles()):
+    charge, sigma, epsilon = nonbonded.getParticleParameters(i)
+    charges.append(charge)
+
+sys_charge = np.round(sum(charges).value_in_unit(elementary_charge), decimals=1)
+## TEST
+#sys_charge = 1.0
+if sys_charge == 0:
+    ## Otherwise sys_charge is -0.0
+    sys_charge = 0.0
+print(sys_charge)
+
+def add_inverted_flat_bottom_restraint(system=system, atom1=5, atom2=14):
+    # Define the custom inverted flat-bottom restraint parameters
+    threshold = 2.0 * nanometers  # Distance threshold
+    spring_constant = 5.0 * kilocalories_per_mole / angstrom**2
+
+    # Define the inverted flat-bottom potential (apply if r < threshold)
+    inverted_flat_bottom_restraint = CustomBondForce(
+        "step(threshold - r) * 0.5 * k * (threshold - r)^2"
+    )
+    inverted_flat_bottom_restraint.addPerBondParameter("threshold")
+    inverted_flat_bottom_restraint.addPerBondParameter("k")
+
+    # Add the restraint between atom1 and atom2
+    inverted_flat_bottom_restraint.addBond(atom1, atom2, [threshold, spring_constant])
+
+    # Add the restraint force to the system
+    system.addForce(inverted_flat_bottom_restraint)
+
+    return(system)
+
+def add_ion(modeller=modeller, ref_index=6, atomic_num=19):
+    # Add an ion at a defined distance from ref atom
+    reference_atom_index = ref_index
+    positions = modeller.getPositions()
+    ref_position = positions[reference_atom_index]
+
+    distance = 2.0 # nanometers
+
+    # Define the normalized direction vector
+    direction = np.array([1.0, 1.0, 1.0])
+    direction /= np.linalg.norm(direction)
+    direction = Vec3(direction[0], direction[1], direction[2])
+
+    # Calculate ion's new position
+    ion_position = ref_position.__add__(direction * distance *nanometers )
+    #print("ion pos ", ion_position )
+
+    # Get the element by atomic number
+    ion_element = Element.getByAtomicNumber(atomic_num) #getBySymbol('K')
+    element_name = ion_element.name
+
+    #    if element_name == "potassium":
+    #        ion_segid = "POT"
+    #
+    #    if element_name == "chlorine":
+    #        ion_segid = "CLL"
+
+    new_topology = Topology()
+    ion_chain = new_topology.addChain("ION")
+    ion_residue = new_topology.addResidue("ION", ion_chain)
+    ion_atom = new_topology.addAtom("ION", ion_element, ion_residue)
+
+    # Append the ion's position to the list of existing positions
+    modeller.add(new_topology, [ion_position ])
+    return(modeller)
+
+if sys_charge ==0:
+    pass
+elif sys_charge <0:
+# Add potassium ion
+    num_particles = system.getNumParticles()
+    print(num_particles)
+    modeller = add_ion(modeller, ref_index=18, atomic_num=19)
+    system = add_inverted_flat_bottom_restraint(system, atom1=18, atom2=19 )
+    system.getNumConstraints()
+elif sys_charge >0:
+# Add chloride ions
+    num_particles = system.getNumParticles()
+    print(num_particles)
+    modeller = add_ion(modeller, ref_index=18, atomic_num=17)
+    system = add_inverted_flat_bottom_restraint(system, atom1=18, atom2=19 )
+    system.getNumConstraints()
+
+modeller.addSolvent(forcefield, model="tip3p", boxSize=Vec3(3.0, 3.0, 3.0)*nanometers, neutralize=False)
+
+# Re-Create OpenMM system
+system = forcefield.createSystem(
+    topology= modeller.topology,
+    nonbondedMethod=PME,
+    nonbondedCutoff=1.2 * nanometers,
+    switchDistance=1.0 * nanometers,
+    constraints=HBonds
+)
+
+nonbonded = [f for f in system.getForces() if isinstance(f, NonbondedForce)][0]
+
+charges = []
+for i in range(system.getNumParticles()):
+    charge, sigma, epsilon = nonbonded.getParticleParameters(i)
+    charges.append(charge)
+
+sys_charge = np.round(sum(charges).value_in_unit(elementary_charge), decimals=1)
+#sys_charge = 1.0
+if sys_charge == 0:
+    ## Otherwise sys_charge is -0.0
+    sys_charge = 0.0
+print(sys_charge)
+
+PDBFile.writeFile(modeller.topology, modeller.positions, open('mol_water.pdb', 'w'))
